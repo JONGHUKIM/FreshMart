959bce21599f8344ab7de765db8545f7f7407c57
JongHuKim <hotsky803@gmail.com>
2024-11-25 16:02:20 +0900

    test

--- lab_freshmart/src/com/itwill/freshmart/view/FreshmartMain.java
+++ lab_freshmart/src/com/itwill/freshmart/view/FreshmartMain.java
@@ -3,9 +3,6 @@ package com.itwill.freshmart.view;
 public class FreshmartMain {
 
 	public static void main(String[] args) {
-		System.out.println("dasf");
-		System.out.println("asf");
-		asgasg
 
 	}
 
