d1429f1bad9f7685fca908862bae36195f489ded
JongHuKim <hotsky803@gmail.com>
2024-11-25 17:37:14 +0900

    Main

---
+++ lab_freshmart/src/com/itwill/freshmart/view/FreshmartMain.java
@@ -0,0 +1,43 @@
+package com.itwill.freshmart.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class FreshmartMain {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FreshmartMain window = new FreshmartMain();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FreshmartMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
