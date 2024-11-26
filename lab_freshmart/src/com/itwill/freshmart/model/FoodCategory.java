84231767896463e9cebf772ecc8a5876adc9b947
JongHuKim <hotsky803@gmail.com>
2024-11-26 16:41:40 +0900

    model

--- lab_freshmart/src/com/itwill/freshmart/model/FoodCategory.java
+++ lab_freshmart/src/com/itwill/freshmart/model/FoodCategory.java
@@ -1,6 +1,12 @@
 package com.itwill.freshmart.model;
 
 public class FoodCategory {
+	
+	   public static final class Entity {
+	        public static final String TBL_FOODCATEGORY = "FOOD_CATEGORY";
+	        public static final String COL_ID = "ID";
+	        public static final String COL_CATEGORY = "CATEGORY";
+	    }
 
 	private Integer id;
 	private String category;
@@ -10,7 +16,6 @@ public class FoodCategory {
 	}
 
 	public FoodCategory(Integer id, String category) {
-		super();
 		this.id = id;
 		this.category = category;
 	}
@@ -29,9 +34,32 @@ public class FoodCategory {
 
 	@Override
 	public String toString() {
-		return "FoodCategory [" + " id = " + id + ", category = " + category + " / " + "]";
+		return "FoodCategory [" + " id = " + id + ", category = " + category + " ]";
 	}
 	
+	  public static CategoryBuilder builder() {
+	        return new CategoryBuilder();
+	    }
 	
+	public static class CategoryBuilder {
+        private Integer id;
+        private String category;
+        
+        private CategoryBuilder() {}
+
+        public CategoryBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public CategoryBuilder category(String category) {
+            this.category = category;
+            return this;
+        }
+
+        public FoodCategory build() {
+            return new FoodCategory(id, category);
+        }
+    }
 
 }
