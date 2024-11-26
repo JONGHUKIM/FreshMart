7f58a81eea16f3f7c16dfd501703068175e09858
JongHuKim <hotsky803@gmail.com>
2024-11-26 17:47:22 +0900

    Jar

--- lab_freshmart/src/com/itwill/freshmart/controller/FreshmartDao.java
+++ lab_freshmart/src/com/itwill/freshmart/controller/FreshmartDao.java
@@ -1,7 +1,39 @@
 package com.itwill.freshmart.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import oracle.jdbc.OracleDriver;
+
 public enum FreshmartDao {
+	INSTANCE;
 	
+	FreshmartDao() {
+        try {
+            
+        	DriverManager.registerDriver(new OracleDriver());
+        	
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 	
+	private void closeResources(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+    private void closeResources(Connection conn, Statement stmt) {
+        closeResources(conn, stmt, null);
+    }
+    
+    
 
 }
