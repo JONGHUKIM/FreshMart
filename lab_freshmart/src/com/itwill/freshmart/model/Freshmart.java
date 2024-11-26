5b11e9c8d3a4b7c64e2b895f6c4fa46adb7f562e
JongHuKim <hotsky803@gmail.com>
2024-11-26 16:48:30 +0900

    freshmartModel

--- lab_freshmart/src/com/itwill/freshmart/model/Freshmart.java
+++ lab_freshmart/src/com/itwill/freshmart/model/Freshmart.java
@@ -1,5 +1,16 @@
 package com.itwill.freshmart.model;
 
 public class Freshmart {
+	
+	public static final class Entity {
+		public static final String TBL_FRESHMART = "FRESHMART";
+		public static final String COL_ID = "ID";
+		public static final String COL_TYPE_ID = "TYPE_ID";
+		public static final String COL_FOOD_NAME = "FOOD_NAME";
+		public static final String COL_EXPIRATION_DATE = "EXPIRATION_DATE";
+		public static final String COL_STORAGE = "STORAGE";
+		public static final String COL_FOOD_QUANTITY = "FOOD_QUANTITY";
+		public static final String COL_IMG = "IMG";
+	}
 
 }
