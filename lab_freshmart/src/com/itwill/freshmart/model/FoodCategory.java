ded82fb2df5f37170788f39f4db3754c68edcb08
JongHuKim <hotsky803@gmail.com>
2024-11-26 15:50:23 +0900

    model

--- lab_freshmart/src/com/itwill/freshmart/model/FoodCategory.java
+++ lab_freshmart/src/com/itwill/freshmart/model/FoodCategory.java
@@ -2,4 +2,36 @@ package com.itwill.freshmart.model;
 
 public class FoodCategory {
 
+	private Integer id;
+	private String category;
+
+	public FoodCategory() {
+
+	}
+
+	public FoodCategory(Integer id, String category) {
+		super();
+		this.id = id;
+		this.category = category;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodCategory [" + " id = " + id + ", category = " + category + " / " + "]";
+	}
+	
+	
+
 }
